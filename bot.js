b04abd4c43b43d3c354c00338064c79a6e4116b6
Noah Bass <bassjnoah@gmail.com>
2015-10-11 10:34:18 -0400

    Initial commit

---
+++ bot.js
@@ -0,0 +1,46 @@
+require('dotenv').load();
+
+var moment = require('moment');
+var fixturesEpl = require('./fixtures/epl.json')['fixtures'];
+var fixturesCl = require('./fixtures/cl.json')['fixtures'];
+var postMessage = require('./lib/postMessage.js');
+
+var matches_rn = [];
+var now        = moment().utc();
+
+fixturesEpl.forEach(function(entry) {
+    // set the entry date to be the entry date in utc and a unix number
+    entry.date = moment(entry.date).utc();
+
+    // if the match is within 10 minutes of the current time,
+    // and it has not been sent, then add the match to the lest of matches_rn
+    if(    (0 <= entry.date - now)
+        && (entry.date.diff(now, 'minutes') <= 10)
+        && (!entry.hasOwnProperty('sent'))
+      ) {
+        matches_rn.push(entry.homeTeamName + ' vs ' + entry.awayTeamName);
+    }
+});
+
+if(matches_rn.length > 0) {
+    if(matches_send == 1) {
+        plural = 'match';
+    }
+    else {
+        plural = 'matches';
+    }
+
+    // post a message using the BOT_ID and matches_rn
+    postMessage(process.env.BOT_ID,
+        matches_rn.length + ' ' + plural + ' kicking off soon:\n' +
+        matches_rn.join(',\n')
+    );
+
+    // set each entry as sent: true
+    matches_rn.forEach(function(entry) {
+        entry.sent: true
+    });
+}
+else {
+    console.log('no matches found');
+}
