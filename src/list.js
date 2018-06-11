d6dbfe422a0df577ebd2102e39abef2e99231ddf
Noah Bass <bassjnoah@gmail.com>
2018-06-10 19:47:40 -0700

    Update list function to return sorted by date

--- src/list.js
+++ src/list.js
@@ -1,10 +1,24 @@
-const sortFixtures = require('./lib/sortFixtures')
 const Fixture = require('./Fixture')
+const moment = require('moment')
+
+/**
+ * Given a list of fixtures, sort by date
+ *
+ * @param   {Array<Fixture>} fixtures
+ * @returns {Array<Fixture>}
+ */
+const sortByDate = (fixtures) => {
+    const sortFixtures = (a, b) => {
+        return moment(a.date).toDate() - moment(b.date).toDate()
+    }
+
+    return fixtures.sort(sortFixtures)
+}
 
 module.exports.list = async (event, context, callback) => {
     try {
         // get fixtures and sort before returning
-        const fixtures = sortFixtures(await Fixture.getFixtures())
+        const fixtures = sortByDate(await Fixture.getFixtures())
         return callback(null, { statusCode: 200, body: JSON.stringify(fixtures) })
     } catch (error) {
         console.error(error)
